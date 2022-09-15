2b0772090ae53e2f2b3469e0e780752250dc08cc
Mustaf Taya <mostfa.taya@gmail.com>
2022-09-15 19:29:21 +0300

    new donate type new donate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\bank\Banks;
 use App\Models\bank\Enterprise;
+use http\Exception\BadMessageException;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -27,14 +28,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        try{
+            $share_listbanks = Banks::with(['enterprise','projects'])->get();
 
-        $share_listbanks = Banks::with(['enterprise','projects'])->get();
+            $share_enterprise = Enterprise::with([
+                'project.city' => function ($query) {
+                    $query->where('inactive', '=', 0);
+                },])->get();
 
-        $share_enterprise = Enterprise::with([
-            'project.city' => function ($query) {
-            $query->where('inactive', '=', 0);
-        },])->get();
-
+        }catch(\Exception $exp) {
+            $share_listbanks=$share_enterprise=array();
+        }
 
         View::share('share_listbank', $share_listbanks);
         View::share('share_enterprise', $share_enterprise);
